c7c4578afee9f8bf274e3da9b371ec66baca36d9
Abhay Tripathi <abhaytripathi270@gmail.com>
2025-12-03 11:57:33 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,26 @@
+a = float(input("Enter first numebr : "))
+b = float(input("Enter the second number : "))
+
+Operation_1 = "+"
+Operation_2 = "-"
+Operation_3 = "*"
+Operation_4 = "/"
+Operation = input("Select the oparation '+','-', '*', '/': ")
+
+if Operation == Operation_1:
+    print(a+b)
+
+if Operation == Operation_2:
+    print(a-b)
+
+if Operation == Operation_3 :
+    print(a*b)
+ 
+if b==0 :
+    print("Cannot divide by zero")
+     
+elif Operation==Operation_4 :
+    print(a/b)
+ 
+else:
+ print("Select correct opration") 
